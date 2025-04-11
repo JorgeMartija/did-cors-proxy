6a4dfb2fca017ce3721f1f99d226a605789601e9
JorgeMartija <63200692+JorgeMartija@users.noreply.github.com>
2025-04-11 17:54:02 +0200

    Update proxy-ice.js

--- api/proxy-ice.js
+++ api/proxy-ice.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Respuesta rápida a preflight
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Only POST allowed' });
   }
@@ -20,6 +28,5 @@ export default async function handler(req, res) {
   });
 
   const data = await response.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(response.status).json(data);
 }
