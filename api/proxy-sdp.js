61012d2922c4e944131146918eaefcdcdacb0f87
JorgeMartija <63200692+JorgeMartija@users.noreply.github.com>
2025-04-11 18:54:10 +0200

    Update proxy-sdp.js

--- api/proxy-sdp.js
+++ api/proxy-sdp.js
@@ -4,16 +4,13 @@ export const config = {
 
 export default async function handler(req) {
   if (req.method !== 'POST') {
-    return new Response(
-      JSON.stringify({ message: 'Only POST allowed' }),
-      {
-        status: 405,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    return new Response(JSON.stringify({ message: 'Only POST allowed' }), {
+      status: 405,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
   }
 
   const apiKey = 'Basic bXVqZXJudWV2YXlvcmtAZ21haWwuY29t:S-4z6mEBXggmFep6ymhBw';
