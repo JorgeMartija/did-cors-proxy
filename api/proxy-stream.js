0fede553ca90af6ba16f104e1680186636eab92d
JorgeMartija <63200692+JorgeMartija@users.noreply.github.com>
2025-04-11 17:55:09 +0200

    Update proxy-stream.js

--- api/proxy-stream.js
+++ api/proxy-stream.js
@@ -1,3 +1,11 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+if (req.method === 'OPTIONS') {
+  return res.status(200).end();
+}
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Only POST allowed' });
